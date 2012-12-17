4517fc7349a4fa9a2f89c04f728340e2fa12a324
Clément Ghnassia <clement.ghnassia@gmail.com>
2012-12-17 20:31:00 +0100

    Oups
    J'avais oublier quelque chose

---
+++ SimCars/src/ConfigCircuit.java
@@ -0,0 +1,5 @@
+
+public class ConfigCircuit {
+	public static int NB_CASES_WIDTH = 10;
+	public static int NB_CASES_HEIGHT = 10;
+}
---
+++ SimCars/src/MapCircuit.java
@@ -0,0 +1,37 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+
+public class MapCircuit {
+	
+	TypeSegment[][] tab;
+	Point start;
+	
+	public MapCircuit() {
+		this.tab = new TypeSegment[ConfigCircuit.NB_CASES_HEIGHT][ConfigCircuit.NB_CASES_WIDTH];
+		start = new Point(0,0);
+	}
+	
+	//créer une map vierge
+	public void init() {
+		for(int i = 0; i < ConfigCircuit.NB_CASES_HEIGHT; i++) {
+			for(int j = 0; j < ConfigCircuit.NB_CASES_WIDTH; i++) {
+				tab[i][j] = TypeSegment.TYPE_NONE;
+			}
+		}
+	}
+	
+	public TypeSegment getSegmentAt(Point p) {
+		return tab[p.y][p.x];
+	}
+	
+	public void setSegmentAt(Point p, TypeSegment type) {
+		this.tab[p.x][p.y] = type;
+	}
+	
+	public ArrayList<Segment> toArray() {
+		ArrayList<Segment> res = new ArrayList<Segment>(); 
+		
+		return res;
+	}
+}
